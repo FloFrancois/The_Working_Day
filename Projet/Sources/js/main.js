6736e0795bfc190f15b59a7ae2258c730891efc1
FloFrancois <florianfrancois4@gmail.com>
2014-04-14 18:27:09 +0200

    cadeau

--- Projet/Sources/js/main.js
+++ Projet/Sources/js/main.js
@@ -5,8 +5,12 @@ window.addEventListener("load",init);
 function init(){
 	if (!game) {
 		var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'gameContainer');
-		game.state.add('classicMode' , WD_game);
-		game.state.start('classicMode');
+		game.state.add('debut' , WD_begin);
+		game.state.add('charger' , WD_load);
+		game.state.add('menu' , WD_menu);
+		game.state.add('jeu' , WD_game);
+		game.state.add('fin' , WD_end);
+		game.state.start('debut');
 	};
 }
 
@@ -16,10 +20,7 @@ var WD_game = function (game) {	}
 WD_game.prototype = {
 
 	preload : function(game) {
-		game.load.image('tache_ordinateur', 'Projet/Sources/assets/turret-1-cannon.gif');
-		game.load.image('tache_dossier', 'Projet/Sources/assets/turret-2-cannon.gif');
-		game.load.image('tache_telephone', 'Projet/Sources/assets/turret-6-cannon.gif');
-		//game.load.spritesheet('effect_dot', 'Projet/Sources/assets/effect_dot.png',40,40);
+
 	},
 
 	//__________________________________________CREATE____________________________________________________________________________________
@@ -32,8 +33,6 @@ WD_game.prototype = {
 		
 		game.time.events.loop(1000, popTache, this, game);
 
-
-
   		//game.style = {font: "bold 15pt Arial", fill: "#ffffff", align: "center", stroke: "#258acc", strokeThickness: 3};
 		//game.hud.life =  game.add.text(10, 10, "Life: "+ game.player.health, game.style);
 	},
@@ -41,11 +40,10 @@ WD_game.prototype = {
 	//__________________________________________UPDATE____________________________________________________________________________________
 
 	update : function (game) {
-
 		for (var i = game.taches.length - 1; i >= 0; i--) {
 			game.taches[i].update(game)
 		};
-
+	
 	},
 
 
