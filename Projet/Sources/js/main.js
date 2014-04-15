635a7398088f76a6263b05e1ebfb089f5e02457f
FloFrancois <florianfrancois4@gmail.com>
2014-04-15 10:05:41 +0200

    Les lignes

--- Projet/Sources/js/main.js
+++ Projet/Sources/js/main.js
@@ -3,18 +3,17 @@ window.addEventListener("load",init);
 
 
 function init(){
-	if (!game) {
-		var game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'gameContainer');
+	// if (!game) {
+		// var 
+		game = new Phaser.Game(1280, 720, Phaser.CANVAS, 'gameContainer');
 		game.state.add('debut' , WD_begin);
 		game.state.add('charger' , WD_load);
 		game.state.add('menu' , WD_menu);
 		game.state.add('jeu' , WD_game);
 		game.state.add('fin' , WD_end);
 		game.state.start('debut');
-	};
+	// };
 }
-
-
 
 var WD_game = function (game) {	}
 WD_game.prototype = {
@@ -30,16 +29,15 @@ WD_game.prototype = {
 		// ################################### CONFIG ########################################
 		game.config = httpGetData('Projet/Sources/config/config.json');
 		game.taches = [];
-		game.employees = [];
-
+		
 		game.time.events.loop(1000, popTache, this, game);
-
-		for (var i = 3 - 1; i >= 0; i--) {
-			game.employees.push(new Employee(game,[200+i*200,200],"red"))
-		};
 
   		//game.style = {font: "bold 15pt Arial", fill: "#ffffff", align: "center", stroke: "#258acc", strokeThickness: 3};
 		//game.hud.life =  game.add.text(10, 10, "Life: "+ game.player.health, game.style);
+
+	    line1 = new Phaser.Line(50, 550, 1000, 550);
+	    line2 = new Phaser.Line(50, 600, 1000, 600);
+	    line3 = new Phaser.Line(50, 650, 1000, 650);
 	},
 
 	//__________________________________________UPDATE____________________________________________________________________________________
@@ -48,29 +46,20 @@ WD_game.prototype = {
 		for (var i = game.taches.length - 1; i >= 0; i--) {
 			game.taches[i].update(game)
 		};
-		for (var i = game.employees.length - 1; i >= 0; i--) {
-			game.employees[i].update(game)
-		};
 	
 	},
-
-
-
-
-
-
 
 	//__________________________________________RENDER____________________________________________________________________________________
 
 	render: function  (game) { 
-
+		game.debug.geom(line1);
+		game.debug.geom(line2);
+		game.debug.geom(line3);
 	}
-
 }
 
 
 //__________________________________________MISC____________________________________________________________________________________
-
 
 /*
 	plus tard on fera que ca pop un tache aléatoirement
@@ -80,5 +69,6 @@ function popTache (game) {
 	var tache = tacheAvaible[(Math.random()*tacheAvaible.length)|0];
 	var possiblePosition = [[50,550],[50,600],[50,650]]
 	var pos = possiblePosition[(Math.random()*possiblePosition.length)|0];
-	game.taches.push(new Tache(game,pos,tache,"red"));
+	game.taches.push(new Tache(game,pos,tache));
 }
+
