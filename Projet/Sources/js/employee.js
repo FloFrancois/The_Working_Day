6346717381c7935eda8b45f838a6d904a8e4ea49
Ttouaty <t.touaty@student.isartdigital.com>
2014-04-15 14:49:08 +0200

    Revert "shit"
    This reverts commit 8ef0ee73951b3037ce84470760430f7d4ca19b39.

---
+++ Projet/Sources/js/employee.js
@@ -0,0 +1,27 @@
+function Employee (game,pos,type,color) {
+	this.type = "employee_"+type;
+	this.color = color;
+	this.sprite = game.add.sprite(pos[0],pos[1],this.type,0);
+
+	for(var attribute in game.config.employees[type]) 
+		this[attribute] = game.config.employees[type][attribute];
+	
+	game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+
+}
+
+Employee.prototype.update = function(game) {
+	for(attribute in game.config.employees[this.type.substr(9)]){
+		if (this[attribute] > game.config.maxCaract)
+			this[attribute] = game.config.maxCaract
+	}
+	for (var i = game.taches.length - 1; i >= 0; i--) {
+		if (game.taches[i].sprite.input.isDragged == false && game.taches[i].color == this.color) {
+			if (game.physics.arcade.overlap(game.taches[i].sprite,this.sprite)) {
+				for(effect in game.taches[i].effects)
+					this[effect] += game.taches[i].effects[effect]
+				game.taches[i].die(game);
+			};
+		};
+	};
+}
---
+++ Projet/Sources/js/taches.js
@@ -0,0 +1,45 @@
+function Tache (game,position,type,color) {
+	this.color = color;
+	this.type = "tache_"+type;
+	this.sprite = game.add.sprite(position[0],position[1],this.type);
+	this.sprite.bringToTop();
+	this.sprite.anchor.setTo(0.5,0.5);
+
+	game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
+	this.sprite.inputEnabled = true;
+	this.sprite.input.enableDrag();
+
+	this.savePos={};
+	this.savePos.x = this.sprite.x;
+	this.savePos.y = this.sprite.y
+
+
+	for(var attribute in game.config.taches[type]) 
+		this[attribute] = game.config.taches[type][attribute];
+}
+Tache.prototype.update = function(game) {
+	if (this.sprite.input.isDragged == false){
+		if(this.sprite.y < 500){
+			this.sprite.x = this.savePos.x;
+			this.sprite.y = this.savePos.y;
+			this.sprite.x += 1;
+			this.savePos.x = this.sprite.x;
+			this.savePos.y = this.sprite.y;
+		}
+		else {
+			this.sprite.x += 1;
+			this.savePos.x = this.sprite.x;
+			this.sprite.y = this.savePos.y;
+		}	
+	}
+	else {
+		this.savePos.x += 1;
+	}
+		
+	if (this.sprite.x > 1000) 
+		this.die(game);
+}
+Tache.prototype.die = function(game) {
+	this.sprite.kill()
+	game.taches.splice(game.taches.indexOf(this),1)
+}
